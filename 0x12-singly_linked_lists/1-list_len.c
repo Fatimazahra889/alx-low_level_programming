6cad7fd12c88d37af752b5dabcf7f6cf15377ed8
fatima zahra sarma <116501204+Fatimazahra889@users.noreply.github.com>
2023-04-28 00:25:30 +0000

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
-
-/**
-  * list_len - Show the number of elements of a list
-  * @h: A linked list
-  *
-  * Return: The number of elements of a list
-  */
-size_t list_len(const list_t *h)
-{
-	size_t count = 0;
-
-	while (h)
+size_t list_len(const list_t *b)
+{size_t count = 0;
+	while (b)
 	{
-		h = h->next;
-		count++;
-	}
-
+		b = b->next;
+		count++;}
 	return (count);
 }
