bb9bccad4cababc5a330a1fb7fff3eeeb77b8e11
fatima zahra sarma <116501204+Fatimazahra889@users.noreply.github.com>
2023-04-28 01:06:09 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,6 +12,8 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
+	
+	
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -19,9 +21,13 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
+		
+		
 		h = h->next;
 		count++;
 	}
 
+	
+	
 	return (count);
 }
