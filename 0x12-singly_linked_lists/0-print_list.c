ae161a05e9810c1dcd32c27c8662650277d870d7
fatima zahra sarma <116501204+Fatimazahra889@users.noreply.github.com>
2023-04-28 00:32:18 +0000

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
-size_t print_list(const list_t *b)
+size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 	while (!= NULL)
 	{
-		if (b->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", b->len, b->str);
-		b=b->next;
+			printf("[%d] %s\n", h->len, h->str);
+		h=h->next;
 		count++;
 	}
 	return (count);}
